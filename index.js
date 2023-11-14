8258acc911c402d4ad6f3c4848353eacc6775122
usupal <udyan.upal@gmail.com>
2023-11-14 13:33:24 +0600

    public url successfully works

--- index.js
+++ index.js
@@ -41,13 +41,34 @@ async function uploadFile() {
 }
 // uploadFile();
 async function deleteFile() {
-    try {
-        const response = await drive.files.delete({
-            fileId: '1daOxTt4qCNtIAWni_3zkheZIn9QhXyeB'
-        })
-        console.log(response.data, response.status)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const response = await drive.files.delete({
+      fileId: "1daOxTt4qCNtIAWni_3zkheZIn9QhXyeB",
+    });
+    console.log(response.data, response.status);
+  } catch (error) {
+    console.log(error);
+  }
 }
 // deleteFile();
+
+async function generatePublicUrl() {
+  try {
+    const fileId = "1HIz6RMyQ8AiX7G4RhBsQRzo4lD6cQ4oE";
+    await drive.permissions.create({
+      fileId: fileId,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+    const result = await drive.files.get({
+      fileId: fileId,
+      fields: "webViewLink, webContentLink",
+    });
+    console.log(result.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+generatePublicUrl();
